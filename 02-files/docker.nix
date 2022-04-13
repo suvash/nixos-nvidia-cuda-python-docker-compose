f15c4e596a5715bbd5506c742899d6b82bde6367
Suvash Thapaliya <suvash@gmail.com>
2022-04-13 22:42:19 +0200

    Update to the latest version of Python & CUDA

--- 02-files/docker.nix
+++ 02-files/docker.nix
@@ -6,7 +6,7 @@
 
   # Nvidia Docker
   virtualisation.docker.enableNvidia = true;
-  # libnvidia-container does not support cgroups v2
+  # libnvidia-container does not support cgroups v2 (prior to 1.8.0)
   # https://github.com/NVIDIA/nvidia-docker/issues/1447
   systemd.enableUnifiedCgroupHierarchy = false;
 }
