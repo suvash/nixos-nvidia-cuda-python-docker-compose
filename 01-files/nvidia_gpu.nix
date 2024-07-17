5d54f6f8e67c8fe68a490e113f21b950c3e14126
Suvash Thapaliya <suvash@gmail.com>
2024-07-17 16:58:57 +0200

    Add opengl driver related options

--- 01-files/nvidia_gpu.nix
+++ 01-files/nvidia_gpu.nix
@@ -3,4 +3,15 @@
 
 {
   services.xserver.videoDrivers = [ "nvidia" ];
+
+  # If you have GPUs from multiple providers (such as Intel)
+  # services.xserver.videoDrivers = [ "intel" "nvidia" ];
+
+  # Also enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
 }
